66fdc5aa449e7570b3afdcc80268882c2d9a3046
Abdul Awal Nadim <nadim.ice.nstu@gmail.com>
2023-02-04 02:46:43 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# ema-john-simple
+
+`https://github.com/ProgrammingHero1/ema-john-resources`
+
+1. Header Component, logo position, set nav menu(48-2)
+   -----------------------TO-----------------------
+
+   `***********set eventhandler, pass eventhandler to child(48-6)`
+
+   `***********Cart state setup, update cart and total on click(48-7)`
+
+   `**********Install external packages, react-fontawesome, display icon(48-8)`
+
+2. Deploy Simple React website to Netlify(48-10)
+
+   00000000000000000000000000000000000888888888888888888888888888888888888888888
+
+Live==> https://regal-longma-5849d3.netlify.app/
---
+++ src/components/Product/Product.js
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import './Product.css';
+
+const Product = ({ product, handleAddToCart }) => {
+  //console.log(props);
+  //const { product, handleAddToCart } = props;
+  //console.log(props.product);
+  const { name, img, seller, price, ratings, quantity } = product;
+
+  return (
+    <div className='product'>
+      <img src={img} alt='' />
+      <div className='product-info'>
+        <p className='product-name'>{name}</p>
+        <p>Price: ${price}</p>
+        <p>
+          <small>Seller: {seller}</small>
+        </p>
+        <p>
+          <small>Ratings: {ratings} stars</small>
+        </p>
+      </div>
+      <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+        <p className='btn-text'>Add to Cart</p>
+        <FontAwesomeIcon icon={faShoppingCart} />
+      </button>
+    </div>
+  );
+};
+
+export default Product;
