2f2262179922741de85f86c37f385de17cf3c2cd
Abdul Awal Nadim <nadim.ice.nstu@gmail.com>
2023-02-04 03:50:35 +0600

    Load cart from local storage, find product

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 1. Run Github clone react project, Create Cart Component(49-2)
    -----------------------TO-----------------------
+   `Setting the cart data on the localStorage for future retrieve(49-4)`
+
+   `Load cart from local storage, find product(49-5)`
 
 2. 00000000000000000000000000000000000099999999999999999999999999999999
 
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -13,10 +14,21 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  useEffect(() => {
+    const storedCart = getStoredCart();
+    //console.log(storedCart);
+    for (const id in storedCart) {
+      //console.log(id);
+      const addedProduct = products.find((product) => product.id === id);
+      console.log(addedProduct);
+    }
+  }, []);
+
   const handleAddToCart = (product) => {
-    console.log(product);
+    // console.log(product);
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id);
   };
 
   return (
